977bef9bb0c900402ffd255612a2da7906cae7a0
JonasB81 <jonas@artndesign.se>
2025-03-05 15:09:10 +0100

    uppdatering av projektr

--- Projekt-inlamning/script.js
+++ Projekt-inlamning/script.js
@@ -1,17 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Ladda in Navbar
-    fetch("navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar-container").innerHTML = data;
-        })
-        .catch(error => console.error("Fel vid inläsning av navbar:", error));
-
-    // Ladda in Footer
-    fetch("footer.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("footer-container").innerHTML = data;
-        })
-        .catch(error => console.error("Fel vid inläsning av footer:", error));
+// Initiera TwentyTwenty när sidan har laddats
+$(window).on("load", function () {
+    $(".twentytwenty-container").twentytwenty({
+        default_offset_pct: 0.5, // Starta med handtaget i mitten
+        orientation: 'horizontal' // Horisontell jämförelse
+    });
+});
+// Mobile Menu 
+document.getElementById("mobile-menu").addEventListener("click", function () {
+    document.querySelector(".navbar-ul").classList.toggle("active");
 });
